0c78a768262c19978720c45cdf26e8c2fc01bd76
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-10 00:50:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+	if(num == 1) return 0;
+	if(num == 2 || num == 3) return 1;
+// your code here 
+	let a=0,b=1,c=a+b;
+	for(let i=3;i<num;i++){
+		if(i == num){
+			return c;
+		}
+		else{
+			let temp = a;
+			a = b;
+			b = c;
+			c = a+b;
+		}
+	}
+	return c;
 }
 
 module.exports = fibonacci;
